3d05a00392d603ac53344eb82c04e622b46ad0ff
sophieouioui <sofapiano20@gmail.com>
2024-10-16 15:48:13 +0300

    added find_by_author_filtered func

--- main.py
+++ main.py
@@ -41,13 +41,32 @@ def filter_book_title(dataset, title):
         if len(book_title_value) > 30:
             ans += 1
 
+    dataset.seek(0)
     return ans
+
+
+def find_by_author_filtered(dataset, title, author):
+    
+    results = []
+    
+    for line in dataset:
+        obj = get_object(line, title)
+        author_value = obj['Book-Author']
+        if author_value == author:
+            if int(obj['Year-Of-Publication']) >= 2018:
+                results.append(obj)
+
+    dataset.seek(0)
+    return results
 
 
 if __name__ == '__main__':
     with open(DATASET_PATH) as dataset:
-        
-        #название длиннее 30 символов
+
+        # название длиннее 30 символов
         title = get_title(dataset)
         print(filter_book_title(dataset, title))
 
+        # поиск книги по автору (от 2018)
+        print(find_by_author_filtered(dataset, title, input()))
+        # но эта функция априори не может работать, т.к. в датасете нет книг после 2018 года
