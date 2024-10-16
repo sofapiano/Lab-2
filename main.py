1a7113f33cfb4c13a02a4dbce321244b7a957ec0
sophieouioui <sofapiano20@gmail.com>
2024-10-16 13:16:17 +0300

    added filter_book_title func

--- main.py
+++ main.py
@@ -1,20 +1,53 @@
-from csv import reader
+import json
+import csv
+
+DATASET_PATH = 'books-en.csv'
+OUT_PATH = 'out.json'
 
 
-flag = 0
-output = open('result.txt', 'w')
-search = input('Search for: ')
-with open('civic.csv', 'r', encoding='windows-1251') as csvfile:
-    table = reader(csvfile, delimiter=';')
-    for row in table:
-        lower_case = row[2].lower()
-        index = lower_case.find(search.lower())
-        if index != -1:
-            print(row[2])
-            flag = 1
-            output.write(f'{row[0]}. {row[2]}. Цена {row[8]} рублей.\n')
+def get_title(dataset):
+    dataset.seek(0)
+    title = next(dataset)
+    title = title.split(';')
+    title = [col.strip() for col in title]
+    return title
 
-    if flag == 0:
-        print('Nothing found.')
 
-output.close()
+def get_object(line, title):
+    reader = csv.DictReader([line], title, delimiter=';', quotechar='"')
+    res = next(reader)
+    return res
+
+
+def filter_year(dataset, title, year):
+    filtered = []
+
+    for line in dataset:
+        obj = get_object(line, title)
+        year_value = obj['Year-Of-Publication']
+        if year_value == str(year):
+            filtered.append(obj)
+
+    dataset.seek(0)
+    return filtered
+
+
+def filter_book_title(dataset, title):
+    ans = 0
+
+    for line in dataset:
+        obj = get_object(line, title)
+        book_title_value = obj['Book-Title']
+        if len(book_title_value) > 30:
+            ans += 1
+
+    return ans
+
+
+if __name__ == '__main__':
+    with open(DATASET_PATH) as dataset:
+        
+        #название длиннее 30 символов
+        title = get_title(dataset)
+        print(filter_book_title(dataset, title))
+
